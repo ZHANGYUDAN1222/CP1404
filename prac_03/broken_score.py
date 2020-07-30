23764aaa558fb7cef2990572169f5d9594cdc3f9
ZHANGYUDAN1222 <68980082+ZHANGYUDAN1222@users.noreply.github.com>
2020-07-30 17:02:26 +0800

    Add files via upload

---
+++ prac_03/broken_score.py
@@ -0,0 +1,38 @@
+<<<<<<< HEAD
+import random
+def result(score):
+    if score < 0 or score >100:
+        print("Invalid score")
+    else:
+        if score >= 90:
+            return 'Excellent'
+        elif score >= 50:
+            return 'Passable'
+        else:
+            return 'Bad'
+def main():
+    score=int(input('Enter the scores:'))
+    score2=random.randint(0,100)
+    sentence1=str(score)+' is '+str(result(score))
+    sentence2=str(score2)+' is '+str(result(score2))
+    print(sentence1,'\n',sentence2)
+=======
+import random
+def result(score):
+    if score < 0 or score >100:
+        print("Invalid score")
+    else:
+        if score >= 90:
+            return 'Excellent'
+        elif score >= 50:
+            return 'Passable'
+        else:
+            return 'Bad'
+def main():
+    score=int(input('Enter the scores:'))
+    score2=random.randint(0,100)
+    sentence1=str(score)+' is '+str(result(score))
+    sentence2=str(score2)+' is '+str(result(score2))
+    print(sentence1,'\n',sentence2)
+>>>>>>> da25c61a7f6cf08fca1fde1d3bd1eac9ac249bfa
+main()
---
+++ prac_03/temperatures.py
@@ -0,0 +1,30 @@
+<<<<<<< HEAD
+def celsius_to_fahrenheit(celsius):                                                                       
+	return celsius * 1.8 + 32.0
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit-32.0)/1.8
+def main():
+    choice=int(input('1 for celsius to fahrenheit\n2 for fahrenheit to celsius\nenter your choice:'))
+    if choice==1:
+        celsius=float(input('Enter celsius temperature:'))
+        print('%s celsius is %.2f fahrenheit'%(celsius,celsius_to_fahrenheit(celsius)))
+    else:
+        fahrenheit=float(input('Enter fahrenheit temperature:'))        
+        print('%s celsius is %.2f fahrenheit'%(fahrenheit,fahrenheit_to_celsius(fahrenheit)))
+if __name__=="__main__":
+=======
+def celsius_to_fahrenheit(celsius):                                                                       
+	return celsius * 1.8 + 32.0
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit-32.0)/1.8
+def main():
+    choice=int(input('1 for celsius to fahrenheit\n2 for fahrenheit to celsius\nenter your choice:'))
+    if choice==1:
+        celsius=float(input('Enter celsius temperature:'))
+        print('%s celsius is %.2f fahrenheit'%(celsius,celsius_to_fahrenheit(celsius)))
+    else:
+        fahrenheit=float(input('Enter fahrenheit temperature:'))        
+        print('%s celsius is %.2f fahrenheit'%(fahrenheit,fahrenheit_to_celsius(fahrenheit)))
+if __name__=="__main__":
+>>>>>>> da25c61a7f6cf08fca1fde1d3bd1eac9ac249bfa
+    main()
