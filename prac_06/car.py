1f6497c4430ed61361999ac47f99ee2feb720433
ZHANGYUDAN1222 <1009040705@qq.com>
2020-08-27 16:53:37 +0800

    Finished the exercise

--- prac_06/car.py
+++ prac_06/car.py
@@ -4,13 +4,14 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, fuel=0, name=""):
         """Initialise a Car instance.
 
         fuel: float, one unit of fuel drives one kilometre
         """
         self.fuel = fuel
         self.odometer = 0
+        self.name=name
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
@@ -29,3 +30,8 @@ class Car:
             self.fuel -= distance
         self.odometer += distance
         return distance
+
+    # Create __str__ method
+    def __str__(self):
+        """Return string representation of Car object"""
+        return("%s, fuel=%i, odometer=%i"%(self.name, self.fuel, self.odometer))
--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -15,5 +15,28 @@ def main():
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
 
+    """ 1. Create a new Car object called "limo" that is initialised with 100 units of fuel."""
+    limo=Car(100)
 
+    """ 2. Add 20 more units of fuel to this new car object using the add method."""
+    limo.add_fuel(20)
+
+    """ 3. Print the amount of fuel in the car."""
+    print("limo fuel:",limo.fuel)
+
+    """ 4. Attempt to drive the car 115km using the drive method."""
+    limo.drive(115)
+
+    """ 5. Print the car's odometer reading."""
+    print("odo:", limo.odometer)
+
+    """ 6. Now add the __str__ method to the Car class in car.py."""
+    print(limo)
+
+    """ 7. Now add a name field to the Car class (in car.py), and adjust the __init__ and __str__ methods to set and display this respectively."""
+    limo=Car(200,"Tesla")
+    limo.drive(150)
+
+    """ 8. Check if __str__ work"""
+    print(limo)
 main()
