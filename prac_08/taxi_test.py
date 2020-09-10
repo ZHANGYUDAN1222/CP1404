a5c7a478169d72aaf5af24a23440eb5e77606c2a
ZHANGYUDAN1222 <1009040705@qq.com>
2020-09-10 16:42:27 +0800

    Restart the meter, drive 100km

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -40,6 +40,8 @@ prius1 = Taxi_P('Prius 1', 100, 1.23)
 prius1.drive(40)
 """3. Print the taxi's details and the current fare"""
 print(prius1.fuel, prius1.get_fare())
-
+"""4. Restart the meter (start a new fare) and then drive the car 100km"""
+prius1.start_fare()
+prius1.drive(100)
 
 
