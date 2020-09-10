8f6284a6d7f163529d8bdcd303f1dce3f30f0ed7
ZHANGYUDAN1222 <1009040705@qq.com>
2020-09-10 17:08:08 +0800

    Modify attribute reliability

--- prac_08/unreliable_car.py
+++ prac_08/unreliable_car.py
@@ -8,7 +8,7 @@ from prac_08.car import Car
 class UnreliableCar(Car):
     """Specialised version of a Car that includes fare costs"""
 
-    def __init__(self, reliability, **kwargs):
+    def __init__(self, reliability=0, **kwargs):
         """Initialise a Taxi instance, based on parent class Car"""
         super().__init__(**kwargs)
         self.reliability = reliability
