df11ed713df7351ffbb684683c8c42fc60a298d7
ZHANGYUDAN1222 <1009040705@qq.com>
2020-09-10 17:22:29 +0800

    Add method Drive, import random.uniform for reliability.

--- prac_08/unreliable_car.py
+++ prac_08/unreliable_car.py
@@ -2,7 +2,7 @@
 CP1404/CP5632 Practical
 Car class
 """
-
+from random import uniform
 from prac_08.car import Car
 
 class UnreliableCar(Car):
@@ -12,3 +12,17 @@ class UnreliableCar(Car):
         """Initialise a Taxi instance, based on parent class Car"""
         super().__init__(**kwargs)
         self.reliability = reliability
+
+    def drive(self, distance):
+        """Drive the car a given distance
+
+        Drive given distance if car has enough fuel
+        or drive until fuel runs out return the distance actually driven.
+        inherited from Car
+        """
+        self.reliability = uniform(0, 100)
+        super().drive(distance)
+        return self.reliability, distance
+
+    def __str__(self):
+        return "{}, fuel={}, reliability={}".format(self.name, self.fuel, self.reliability)
