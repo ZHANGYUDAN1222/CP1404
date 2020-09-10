3235527c898748cbbc7d725460a6ea0cdad93437
ZHANGYUDAN1222 <1009040705@qq.com>
2020-09-10 23:05:36 +0800

    Add class variable flagfall

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -8,6 +8,9 @@ from prac_08.taxi import Taxi
 class SilverServiceTaxi(Taxi):
     """Specialised version of a Taxi that is a Car."""
 
+    """Add class variable flagfall"""
+    flagfall = 4.5
+
     """Add a new attribute, fanciness, which is a float that scales the price_per_km"""
     def __init__(self, name='', fuel=0, finances=0):
         super().__init__(name, fuel)
