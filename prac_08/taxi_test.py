8ce402333de4f347d587f25126870ea14c25e6fb
ZHANGYUDAN1222 <1009040705@qq.com>
2020-09-10 16:40:23 +0800

    Create a new instance Prius 1

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,41 @@
+"""
+CP1404 Practical
+Car class
+"""
+from prac_08.car import Car
+from prac_08.taxi import Taxi
+
+class Taxi_P(Taxi, Car):
+    """Specialised version of a Car that includes fare costs"""
+
+    def __init__(self, name, fuel, price_per_km):
+        """Initialise a Taxi instance, based on parent class Car"""
+        super().__init__(name, fuel, price_per_km)
+        self.current_fare_distance = 0
+
+    def __str__(self):
+        """Return a string like a Car but with current fare distance"""
+        return "{}, {}km on current fare, ${:.2f}/km".format(super().__str__(), self.current_fare_distance, self.price_per_km)
+
+    def drive(self, distance):
+        """Drive the car a given distance.
+
+        Drive given distance if car has enough fuel
+        or drive until fuel runs out return the distance actually driven.
+        """
+        super().drive(distance)
+
+    def get_fare(self):
+        """Return the price for the taxi trip."""
+        return self.price_per_km * self.current_fare_distance
+
+    def start_fare(self):
+        """Begin a new fare."""
+        self.current_fare_distance = 0
+
+
+"""1. Create a new taxi with name "Prius 1", 100 units of fuel and price of $1.23/km"""
+prius1 = Taxi_P('Prius 1', 100, 1.23)
+
+
+
