085fb28a0b27dceb4c21d1f1f97c399f08c0951e
ZHANGYUDAN1222 <1009040705@qq.com>
2020-09-10 16:53:09 +0800

    Remove price_per_km in __init__

--- prac_08/taxi.py
+++ prac_08/taxi.py
@@ -11,10 +11,10 @@ class Taxi(Car):
     """Add class variable"""
     price_per_km = 1.23
 
-    def __init__(self, name, fuel, price_per_km):
+    """Remove the price_per_km"""
+    def __init__(self, name, fuel):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
-        self.price_per_km = price_per_km
         self.current_fare_distance = 0
 
     def __str__(self):
