4310ecfa038a2e6f14fbecef5144aa9f41f2c32f
ZHANGYUDAN1222 <1009040705@qq.com>
2020-09-14 20:24:22 +0800

    Add flagfall to get fare

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -19,5 +19,10 @@ class SilverServiceTaxi(Taxi):
         self.price_per_km = self.finances * Taxi.price_per_km
         self.current_fare_distance = 0
 
+    """Add flagfall to the fare"""
+    def get_fare(self):
+        """Return the price for the taxi trip."""
+        return (self.price_per_km * self.current_fare_distance)+ self.flagfall
+
     def __str__(self):
         return super().__str__() + " plus flagfall of ${}".format(self.flagfall)
