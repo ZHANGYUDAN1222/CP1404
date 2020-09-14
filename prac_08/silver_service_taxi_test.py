743060a2c733aee6bdb2efdf51442ea7921bc217
ZHANGYUDAN1222 <1009040705@qq.com>
2020-09-14 20:07:45 +0800

    Import and create instance Tesla

---
+++ prac_08/silver_service_taxi_test.py
@@ -0,0 +1,12 @@
+"""
+CP1404/CP5632 Practical
+Taxi class
+"""
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+def main():
+    """test 18km trip in a SilverServiceTaxi with fanciness of 2, the fare should be $48.78 (yikes!)"""
+
+    """Create a new taxi with name "tesla", 100 units of fuel and fanciness of 2"""
+    tesla= SilverServiceTaxi("Tesla", 100, 2)
+
