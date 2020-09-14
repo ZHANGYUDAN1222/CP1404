186f0f1d97239d0056d1299f13fbf8673858c988
ZHANGYUDAN1222 <1009040705@qq.com>
2020-09-14 20:25:24 +0800

    Test fare

--- prac_08/silver_service_taxi_test.py
+++ prac_08/silver_service_taxi_test.py
@@ -8,5 +8,12 @@ def main():
     """test 18km trip in a SilverServiceTaxi with fanciness of 2, the fare should be $48.78 (yikes!)"""
 
     """Create a new taxi with name "tesla", 100 units of fuel and fanciness of 2"""
-    tesla= SilverServiceTaxi("Tesla", 100, 2)
+    tesla = SilverServiceTaxi("Tesla", 100, 2)
+    """Drive 18km"""
+    tesla.drive(18)
+    """Test fare"""
+    print(tesla.get_fare())
+    """test __str__"""
+    print(tesla)
 
+main()
