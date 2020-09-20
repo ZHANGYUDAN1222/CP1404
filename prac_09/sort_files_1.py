7c90fc149d9c10b5f3eb9d8954695a3a1e34a754
ZHANGYUDAN1222 <1009040705@qq.com>
2020-09-20 19:27:39 +0800

    Modify function sort_file() to delete the '.' in select condition

--- prac_09/sort_files_1.py
+++ prac_09/sort_files_1.py
@@ -6,25 +6,42 @@ Create new directories and Sort extensions
 import os
 
 def main():
+    txt = []
+    doc = []
+    docx = []
+    xls = []
+    xlsx = []
+    png = []
+    gif = []
+    jpg = []
     """Get current working directory"""
     print("Starting directory is: {}".format(os.getcwd()))
+    """Change path to directory FilesToSort"""
+    os.chdir('FilesToSort')
+    for filename in os.listdir('.'):
+        txt, doc, docx, xls, xlsx, png, gif, jpg = sort_files(filename,txt, doc, docx, xls, xlsx, png, gif, jpg)
+    print(txt, doc, docx, xls, xlsx, png, gif, jpg)
 
 def sort_files(filename,txt, doc, docx, xls, xlsx, png, gif, jpg):
     split_name = filename.split('.')
-    if split_name[1] == '.txt':
+    if split_name[1] == 'txt':
         txt.append(filename)
-    elif split_name[1] == '.doc':
+    elif split_name[1] == 'doc':
         doc.append(filename)
-    elif split_name[1] == '.docx':
+    elif split_name[1] == 'docx':
         docx.append(filename)
-    elif split_name[1] == '.xls':
+    elif split_name[1] == 'xls':
         xls.append(filename)
-    elif split_name[1] == '.xlsx':
+    elif split_name[1] == 'xlsx':
         xlsx.append(filename)
-    elif split_name[1] == '.png':
+    elif split_name[1] == 'png':
         png.append(filename)
-    elif split_name[1] == '.gif':
+    elif split_name[1] == 'gif':
         gif.append(filename)
-    elif split_name[1] == '.jpg':
+    elif split_name[1] == 'jpg':
         jpg.append(filename)
+    else:
+        print('Wrong')
     return txt, doc, docx, xls, xlsx, png, gif, jpg
+
+main()
