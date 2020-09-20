90e626d9240979c1797db4211f81f66ee796ddf9
ZHANGYUDAN1222 <1009040705@qq.com>
2020-09-21 02:26:28 +0800

    Modify main(),sort_files(),change list to dictionary

--- prac_09/sort_files_1.py
+++ prac_09/sort_files_1.py
@@ -2,51 +2,38 @@
 CP1404/CP5632 Practical
 Create new directories and Sort extensions
 """
-
+import shutil
 import os
 
 def main():
     """Get current working directory"""
     print("Starting directory is: {}".format(os.getcwd()))
+
     """Change path to directory FilesToSort"""
     os.chdir('FilesToSort')
-    """Create lists to sort different files"""
-    txt = []
-    doc = []
-    docx = []
-    xls = []
-    xlsx = []
-    png = []
-    gif = []
-    jpg = []
+
+    """Create dictionary to store different files"""
+    all_type = {'txt': [], 'doc': [], 'docx': [], 'xls': [], 'xlsx': [], 'png': [], 'gif': [], 'jpg': []}
+
     """Sort different files in corresponding list"""
     for filename in os.listdir('.'):
-        txt, doc, docx, xls, xlsx, png, gif, jpg = sort_files(filename,txt, doc, docx, xls, xlsx, png, gif, jpg)
-    # Test if the file in the right list
-    # print(txt, doc, docx, xls, xlsx, png, gif, jpg)
+        if os.path.isfile(filename):
+            all_type = sort_files(filename, all_type)
+        else:
+            pass
+    # test if all_type returns correctly
+    # print(all_type)
 
-def sort_files(filename,txt, doc, docx, xls, xlsx, png, gif, jpg):
+def sort_files(filename,all_type):
     """Function to sort and append files according to there type"""
     # create a list , split by '.'
     split_name = filename.split('.')
-    if split_name[1] == 'txt':
-        txt.append(filename)
-    elif split_name[1] == 'doc':
-        doc.append(filename)
-    elif split_name[1] == 'docx':
-        docx.append(filename)
-    elif split_name[1] == 'xls':
-        xls.append(filename)
-    elif split_name[1] == 'xlsx':
-        xlsx.append(filename)
-    elif split_name[1] == 'png':
-        png.append(filename)
-    elif split_name[1] == 'gif':
-        gif.append(filename)
-    elif split_name[1] == 'jpg':
-        jpg.append(filename)
-    else:
-        print('Wrong')
-    return txt, doc, docx, xls, xlsx, png, gif, jpg
+    for k,v in all_type.items():
+        if split_name[1] == k:
+            v.append(filename)
+        else:
+            continue
+    return all_type
+
 
 main()
